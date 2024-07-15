3d78f672e8cfd60d9442212919cd132effcb1e48
BassManLuke <luke_haz@yahoo.co.uk>
2024-07-15 11:59:16 +0100

    remove console logs

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -3,12 +3,10 @@ const { fetchTopics } = require("../models/topics.models")
 function getTopics(request, response, next) {
     fetchTopics()
     .then(({rows}) => {
-        // console.log(rows, "<< Rows")
         const topics = rows
         response.status(200).send({topics})
     })
     .catch((error) => {
-        console.log(error, "<<< Err in Controller")
         next(error)
     })
 }
