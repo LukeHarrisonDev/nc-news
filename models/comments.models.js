b783c69b82754cf7624407c10338c2fc9fa0975d
BassManLuke <luke_haz@yahoo.co.uk>
2024-07-17 13:40:02 +0100

    refactor the comments model

--- models/comments.models.js
+++ models/comments.models.js
@@ -16,18 +16,17 @@ function fetchComments(id) {
 }
 
 function insertComment(newComment, article_id) {
-    if (Object.keys(newComment).length === 0 || newComment.body.length === 0) {
+    if (Object.keys(newComment).length <= 1 || newComment.body.length === 0) {
         return Promise.reject({status:400, message: "Bad request"})
     }
+    
     let sqlString =
     `INSERT INTO comments (author, body, article_id)
-    VALUES %L
+    VALUES ($1, $2, $3)
     RETURNING *`
 
-    const formattedValues = [[newComment.username, newComment.body, article_id.article_id]]
-
-    const queryString = format(sqlString, formattedValues)
-    return db.query(queryString)
+    const values = [newComment.username, newComment.body, article_id.article_id]
+    return db.query(sqlString, values)
     .then (({rows}) => {
         return rows[0]
     })
