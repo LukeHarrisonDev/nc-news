1075f80a8550701ce9e0cbfd035694fb4ccaa362
BassManLuke <luke_haz@yahoo.co.uk>
2024-07-17 08:42:17 +0100

    add POST functionality for comments, with test suite

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const { getArticleById, getArticles } = require("./controllers/articles.controllers")
-const { getComments } = require("./controllers/comments.controllers")
+const { getComments, postComment } = require("./controllers/comments.controllers")
 const { getTopics } = require("./controllers/topics.controllers")
 const endpoints = require("./endpoints.json")
 const {
     catchInvalidEndpoints,
-    invalidDataTypeError,
-    outOfRangeError,
+    badRequestError,
+    notFoundError,
     customError,
     catchAllErrors} = require("./error-handling")
 
 const express = require("express")
 const app = express()
+app.use(express.json())
 
 app.get("/api", (request, response) => {
     response.status(200).send({endpoints})
@@ -23,14 +24,14 @@ app.get("/api/articles", getArticles)
 app.get("/api/articles/:article_id", getArticleById)
 
 app.get("/api/articles/:article_id/comments", getComments)
+app.post("/api/articles/:article_id/comments", postComment)
 
 app.all("/*", catchInvalidEndpoints)
 
-
 //Error Handling
-app.use(invalidDataTypeError)
+app.use(badRequestError)
 
-app.use(outOfRangeError)
+app.use(notFoundError)
 
 app.use(customError)
 
--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,14 +1,26 @@
-const { fetchComments } = require("../models/comments.models")
+const { fetchComments, insertComment } = require("../models/comments.models")
 
 function getComments(request, response, next) {
     const id = request.params.article_id
-fetchComments(id)
-.then((comments) => {
-    response.status(200).send({comments})
-})
-.catch((error) => {
-    next(error)
-})
+    fetchComments(id)
+    .then((comments) => {
+        response.status(200).send({comments})
+    })
+    .catch((error) => {
+        next(error)
+    })
 }
 
-module.exports = {getComments}
+function postComment(request, response, next) {
+    const newComment = request.body
+    const articleId = request.params
+    insertComment(newComment, articleId)
+    .then((comment) => {
+        response.status(201).send({comment})
+    })
+    .catch((error) => {
+        next(error)
+    })
+}
+
+module.exports = {getComments, postComment}
